95b8153dd0195453ceb4ee9279aaabb53563030f
Rob Cooney <robertcooney99@gmail.com>
2022-12-02 21:05:05 +0000

    AOC-2022: Day 001 Part 001 Solution

---
+++ solutions/day001part001.py
@@ -0,0 +1,15 @@
+# ADVENT OF CODE 2022: DAY 001 PART 001
+# 
+# puzzle input: input is a text file of elves' inventories
+# containing the number of calories of food items, each
+# elves inventory (if any) by a blank line.
+#
+# puzzle output: the number of calories of the elf
+# with the highest number of calories.
+
+from utils import aochelper
+
+input = aochelper.txtToString("1")
+
+elves = list(map(lambda basket: sum(map(int, basket.split("\n"))), input.split("\n\n")))
+print(f"Highest calories: {sorted(elves)[-1]}")
