4077eb3710d2e75ece4cd4ec0efba13f5ea59c9b
Rob Cooney <robertcooney99@gmail.com>
2022-12-02 21:10:06 +0000

    AOC-2022: Day 001 Part 002 Solution

---
+++ solutions/day001part002.py
@@ -0,0 +1,15 @@
+# ADVENT OF CODE 2022: DAY 001 PART 002
+# 
+# puzzle input: input is a text file of elves' inventories
+# containing the number of calories of food items, each
+# elves inventory (if any) by a blank line.
+#
+# puzzle output: the total number of calories carried by
+# the three elves with the highest number of calories.
+
+from utils import aochelper
+
+input = aochelper.txtToString("1")
+
+elves = list(map(lambda basket: sum(map(int, basket.split("\n"))), input.split("\n\n")))
+print(f"Calorie total for top 3 elves combined:{sum(sorted(elves, reverse=True)[:3])}")
