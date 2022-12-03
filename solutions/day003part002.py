d06722818cc9995407c7afe39a09ba068481ac29
Rob Cooney <robertcooney99@gmail.com>
2022-12-03 17:56:34 +0000

    AOC-2022: Day 003 Part 002 Solution

---
+++ solutions/day003part002.py
@@ -0,0 +1,24 @@
+# ADVENT OF CODE 2022: DAY 003 PART 002
+# 
+# puzzle input: text file
+#
+# puzzle output: priority total
+
+from utils import aochelper
+
+input = aochelper.txtToString("3")
+
+rucksacks = list(map(lambda rucksack: "".join(set(rucksack)),input.split("\n")))
+grouped_rucksacks = [rucksacks[x:x+3] for x in range(0, len(rucksacks), 3)]
+
+badges = []
+for rucksack_group in grouped_rucksacks:
+    for item in rucksack_group[0]:
+        if item in rucksack_group[1] and item in rucksack_group[2]:
+            badges += item
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+priorityTotal = sum(list(map(lambda letter: alphabet.index(letter) + 1, badges)))
+
+print(f"Priority total: {priorityTotal}")
+    
