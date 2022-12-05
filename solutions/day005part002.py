b2c71bb93c4f7a48d82f17c0ce17344712ccf343
Rob Cooney <robertcooney99@gmail.com>
2022-12-05 21:34:01 +0000

    AOC-2022: Day 005 Part 002 Solution

---
+++ solutions/day005part002.py
@@ -0,0 +1,37 @@
+# ADVENT OF CODE 2022: DAY 005 PART 002
+# 
+# puzzle input: input is a text file containing two
+# parts. the first part is number of stacked crates,
+# the second part is instructions on how to move the
+# crates. in this part of the puzzle crates are moved
+# from the top of the stacks together.
+#
+# puzzle output: after completing all the instructions
+# list the crates (letters) at the top of the stacks.
+
+from utils import aochelper
+
+input = aochelper.text_to_string("5")
+
+crates, instructions = input.split("\n\n")
+crates = crates.split("\n")
+crates.reverse()
+
+stacks = {k: [] for k in range(1,10)}
+
+for crate in crates[1:]:
+    for x in range(0, int(len(crate) / 4) + 1):
+        value = crate[x*4:(x*4)+3].strip()
+        if value:
+            stacks[x+1].append(value[1])
+
+for instruction in instructions.split("\n"):
+    details = instruction.split(" ")
+    amount_to_move, section_from, section_to = int(details[1]), int(details[3]), int(details[5])
+    crates_to_move = stacks[section_from][-amount_to_move:]
+    for crate in crates_to_move:
+        stacks[section_to].append(crate)
+    del stacks[section_from][-amount_to_move:]
+
+top_crates = "".join(map(lambda stack: stack[-1], stacks.values()))
+print(f"Crates at top of stacks: {top_crates}")
